acb4d90ba0db5eed803981c40ba8075be5d8d236
yellenkibhavyasri <bhavyasriyellenki@gmail.com>
2026-04-10 22:09:58 +0530

    HTTP Methods

--- index.js
+++ index.js
@@ -3,14 +3,24 @@ const fs=require("fs");
 const url =require("url");
 const myServer=http.createServer((req,res)=>{
     if(req.url==="/favicon.ico") return res.end();
-    const log=`${Date.now()}: ${req.url} New Req Received\n`;
+    const log=`${Date.now()}: ${req.method}${req.url} New Req Received\n`;
     const myurl=url.parse(req.url,true);
-    console.log(myurl);
+    
     fs.appendFile('log.txt',log,(err,data)=>{
         switch(req.url){
-            case '/':res.end("HomePage");
+            case '/':
+                if(req.method==="GET") 
+                    res.end("HomePage");
             break;
-            case '/about':res.end("Iam Bhavya");
+            case '/about':
+                
+                res.end("Iam Bhavya");
+            break;
+            case '/signup':
+                if(req.method==='GET') res.end("This is a signup Form");
+                else if(req.method==='POST'){
+                    res.end("Success");
+                }
             break;
             default:
                 res.end("404 Not Found");
