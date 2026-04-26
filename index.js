8b0cd9f65b48ef6fda975f65fa4af121df99937a
yellenkibhavyasri <bhavyasriyellenki@gmail.com>
2026-04-26 21:30:09 +0530

    Introduction to express
    Intro to express

--- index.js
+++ index.js
@@ -1,31 +1,14 @@
 const http=require("http");
-const fs=require("fs");
-const url =require("url");
-const myServer=http.createServer((req,res)=>{
-    if(req.url==="/favicon.ico") return res.end();
-    const log=`${Date.now()}: ${req.method}${req.url} New Req Received\n`;
-    const myurl=url.parse(req.url,true);
-    
-    fs.appendFile('log.txt',log,(err,data)=>{
-        switch(req.url){
-            case '/':
-                if(req.method==="GET") 
-                    res.end("HomePage");
-            break;
-            case '/about':
-                
-                res.end("Iam Bhavya");
-            break;
-            case '/signup':
-                if(req.method==='GET') res.end("This is a signup Form");
-                else if(req.method==='POST'){
-                    res.end("Success");
-                }
-            break;
-            default:
-                res.end("404 Not Found");
-        }
-        
-    });
+//const fs=require("fs");
+//const url =require("url");
+const express=require("express");
+const app=express();
+app.get('/',(req,res)=>{
+    return res.send("Hello From Home Page");
 });
+app.get('/about',(req,res)=>{
+    return res.send("Hello From About Page");
+});
+
+const myServer=http.createServer(app);
 myServer.listen(8000,()=>console.log("Server Started"));
