a076aaaf5d4506708b285097f2d56f79e10fe7b8
yellenkibhavyasri <bhavyasriyellenki@gmail.com>
2026-05-06 15:27:23 +0530

    Building rest api using node.js and express.js
    Building rest api using node.js and express.js

--- index.js
+++ index.js
@@ -1,14 +1,31 @@
-const http=require("http");
-//const fs=require("fs");
-//const url =require("url");
-const express=require("express");
-const app=express();
-app.get('/',(req,res)=>{
-    return res.send("Hello From Home Page");
-});
-app.get('/about',(req,res)=>{
-    return res.send("Hello From About Page");
-});
+const express= require('express');
+const users=require("./MOCK_DATA.json");
 
-const myServer=http.createServer(app);
-myServer.listen(8000,()=>console.log("Server Started"));
+const app=express();
+const PORT=8000;
+app.get('/users',(req,res)=>{
+    const html=`
+    <ul>
+    ${users.map(user=>`<li>${user.first_name}</li>`).join("")}
+    </ul>
+    `;
+    res.send(html);
+});
+app.get("/api/users",(req,res)=>{
+    return res.json(users);
+});
+app.get("/api/users/:id",(req,res)=>{
+    const id=Number(req.params.id);
+    const user=users.find((user)=> user.id===id);
+    return res.json(user);
+});
+app.post("/api/users",(req,res)=>{
+    return res.json({status:"pending"});
+});
+app.patch("/api/users/:id",(req,res)=>{
+    return res.json({status:"pending"});
+});
+app.delete("/api/users/:id",(req,res)=>{
+    return res.json({status:"pending"});
+});
+app.listen(PORT,()=> console.log(`Server Strated at Port`));
